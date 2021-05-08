c0924e6a017f9845fdc1358c20d5fd8d5fd68a4a
Tim Hoehenleitner <tim.hoehenleitner@hm.edu>
2021-05-08 12:30:46 +0200

    Added and organised DTOs

---
+++ src/SEIIApp/Shared/DomainDTOs/DocumentContentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEIIApp.Shared.DomainDTOs
+{
+    public class DocumentContentDto
+    {
+        public int ContentId { get; set; }
+        public string Title { get; set; }
+        public List<LessonDto> Lessons { get; set; }
+        public Uri Location { get; set; }
+    }
+}
