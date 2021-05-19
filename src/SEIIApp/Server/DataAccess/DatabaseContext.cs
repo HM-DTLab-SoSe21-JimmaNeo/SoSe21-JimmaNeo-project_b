a7e4e49ccf35b78793c1f1fc482fb4529721cd44
lars-k <Lars.Kriese@pari.com>
2021-05-19 17:01:16 +0200

    [Backend] Refactor
    * Remove not needed WeatherForcast items
    * Remove todo-Comment

--- src/SEIIApp/Server/DataAccess/DatabaseContext.cs
+++ src/SEIIApp/Server/DataAccess/DatabaseContext.cs
@@ -11,8 +11,6 @@ namespace SEIIApp.Server.DataAccess
         }
 
         public DbSet<Course> Courses { get; set; }
-
-        //TODO: Lessons ist ja über Courses definiert, aber wenn ich nur eine bestimmte Lesson muss ich sie hier auch definieren.
         public DbSet<Lesson> Lessons { get; set; }
         public DbSet<Quiz> Quizzes { get; set; }
         public DbSet<Student> Students { get; set; }
