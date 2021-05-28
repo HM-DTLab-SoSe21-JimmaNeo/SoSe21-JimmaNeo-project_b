1f1b5c5feb8ea203fe424e015dc7c3f8c87198fe
Tim Hoehenleitner <tim.hoehenleitner@hm.edu>
2021-05-28 11:38:17 +0200

    [Backend] Added AvatarService and AvatarController

---
+++ src/SEIIApp/Server/Services/AvatarService.cs
@@ -0,0 +1,38 @@
+﻿using SEIIApp.Server.DataAccess;
+using SEIIApp.Server.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using AutoMapper;
+
+namespace SEIIApp.Server.Services
+{
+    public class AvatarService
+    {
+        private DatabaseContext DatabaseContext { get; set; }
+        IMapper Mapper { get; set; }
+        public AvatarService(DatabaseContext db, IMapper mapper)
+        {
+            this.DatabaseContext = db;
+            this.Mapper = mapper;
+        }
+
+        private IQueryable<Avatar> GetQueryableForAvatars()
+        {
+            return DatabaseContext
+                .Avatars
+                .Include(avatar => avatar.UsedItems);
+        }
+
+        public Avatar[] GetAllAvatars()
+        {
+            return GetQueryableForAvatars().ToArray();
+        }
+
+        public Avatar GetAvatarWithId(int avatarId)
+        {
+            return GetQueryableForAvatars().Where(avatar => avatar.AvatarId == avatarId).FirstOrDefault();
+        }
+
+       
+    }
+}
