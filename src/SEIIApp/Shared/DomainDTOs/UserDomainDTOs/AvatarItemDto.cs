9cc964015b011fda4fdca5423536e5029a6cf748
Simon Imhof <simon.imhof1@googlemail.com>
2021-06-03 18:09:47 +0200

    [Frontend] Profile Updated

--- src/SEIIApp/Shared/DomainDTOs/UserDomainDTOs/AvatarItemDto.cs
+++ src/SEIIApp/Shared/DomainDTOs/UserDomainDTOs/AvatarItemDto.cs
@@ -2,7 +2,7 @@
 {
     public class AvatarItemDto
     {
-        public string ItemId { get; set; }
+        public int ItemId { get; set; }
         public string Source { get; set; }
         public string SourceButton { get; set; }
     }
